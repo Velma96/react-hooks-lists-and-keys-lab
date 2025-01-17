0e127531c18241331848de937102c4e1e9a7c6fa
phoebe velma awuor <awuorphoebi@gmail.com>
2025-01-17 10:55:22 +0300

    complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {/* Map over the links array to create <a> tags */}
+      {links.map((link, index) => (
+        <a key={index} href={`#${link}`}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  );
+
 }
 
 export default NavBar;
